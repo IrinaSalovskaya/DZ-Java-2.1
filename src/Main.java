40c83c4525836dd4fb82e14fa95b0a23ce4d2388
Irina Salovskaya <salovskaya@yandex.ru>
2021-03-09 17:44:38 +0300

    Added Main Java

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int depozit = 500_000_000;
+        int total = balance+ depozit;
+        System.out.println(total);
+    }
