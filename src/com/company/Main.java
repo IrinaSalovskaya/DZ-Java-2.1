c5560255e1bbf577cb8327a522b85cb05d189a60
Irina Salovskaya <salovskaya@yandex.ru>
2021-03-09 11:06:23 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean registered = true;
+        float amount = 1000.60F;
+        float percent = 0.03F;
+
+        float bonus = amount * percent;
+        System.out.println(bonus);
+    }
+}
